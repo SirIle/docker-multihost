17aa0bdd1c8e85d668096905ca4dbc2f3dac0a29
Ilkka Anttonen <ilkka.anttonen@accenture.com>
2015-07-28 19:15:17 +0300

    Added Consul as the default DNS for service containers.

--- swarm/startService.sh
+++ swarm/startService.sh
@@ -6,9 +6,9 @@ eval $(docker-machine env --swarm swarm-master)
 HAPROXY=$(docker ps | awk '{print $1,$2}' | grep haproxy | wc -l)
 if [[ $HAPROXY -lt 1 ]]; then
   echo "** At least one instance of HAProxy needs to be running, starting **"
-  docker run -d -h rest --name=rest -e SERVICE_NAME=rest -p 80:80 -p 1936:1936 sirile/haproxy -consul=$(docker-machine ip infra):8500
+  docker run -d -h rest --name=rest -e SERVICE_NAME=rest --dns 172.17.42.1 -p 80:80 -p 1936:1936 sirile/haproxy -consul=$(docker-machine ip infra):8500
 fi
 echo "** Starting image $2 with the name $1 **"
-docker run -d -e SERVICE_NAME=$1 -e SERVICE_TAGS=rest -p :80 $2
+docker run -d -e SERVICE_NAME=$1 -e SERVICE_TAGS=rest --dns 172.17.42.1 -p :80 $2
 REST_ADDR=$(docker inspect $(docker ps -f name=rest -q) | grep IP\" | cut -d '"' -f 4)
 echo "** Service available at http://$REST_ADDR/$1 **"
