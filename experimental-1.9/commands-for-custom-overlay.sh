88f7a2396a73a8bfef96d74cba859ba399db25a5
Ilkka Anttonen <ilkka.anttonen@accenture.com>
2015-10-05 03:01:22 +0300

    Got registrator to work
    Running with -internal seems to do the trick inside overlay. Figured this
    out after looking through the registrator source code.

--- experimental-1.9/commands-for-custom-overlay.sh
+++ experimental-1.9/commands-for-custom-overlay.sh
@@ -17,25 +17,19 @@ docker-machine create -d virtualbox --engine-opt="default-network=overlay:multih
 docker-1.9 $(docker-machine config frontend) run -d --name frontend -p 8500:8500 progrium/consul -server -bootstrap-expect 1
 
 # Start registrator on frontend node, idea is to register the public IP for the frontend services
-#docker-1.9 $(docker-machine config frontend) run -d -v /var/run/docker.sock:/tmp/docker.sock -h registrator --name registrator gliderlabs/registrator consul://frontend:8500
+docker-1.9 $(docker-machine config frontend) run --rm -v /var/run/docker.sock:/tmp/docker.sock -h registrator --name registrator gliderlabs/registrator -internal consul://frontend:8500
 
 # Start haproxy on frontend node
 docker-1.9 $(docker-machine config frontend) run -d --name=rest -p 80:80 -p 1936:1936 sirile/haproxy -consul=frontend:8500
 
 # Start up a few example services with an exposed port
-CONTAINER=$(docker run -d --dns 172.17.42.1 sirile/node-image-test)
-# Register the started service manually
-curl -X post -d '{"Name":"test","ID":"'$CONTAINER'","Tags":["rest"],"Address":"'$(docker inspect --format="{{.NetworkSettings.IPAddress}}" $CONTAINER)'", "port":80}' http://$(docker-machine ip frontend):8500/v1/agent/service/register
-
-# Dry run:
-# docker run --dns 172.17.42.1 --rm sirile/haproxy -consul=consul.service.consul:8500 -dry -once
+docker-1.9 run -d -e SERVICE_NAME=test -e SERVICE_TAGS=rest --dns 172.17.42.1 -p :80 sirile/node-image-test
+docker-1.9 run -d -e SERVICE_NAME=test -e SERVICE_TAGS=rest --dns 172.17.42.1 -p :80 sirile/node-image-test
+docker-1.9 run -d -e SERVICE_NAME=test -e SERVICE_TAGS=rest --dns 172.17.42.1 -p :80 sirile/node-image-test
 
 # Create application node
 docker-machine create -d virtualbox --engine-opt="default-network=overlay:multihost" --engine-opt="kv-store=consul:$(docker-machine ip infra):8500" --engine-label="com.docker.network.driver.overlay.bind_interface=eth1" --engine-label="com.docker.network.driver.overlay.neighbor_ip=$(docker-machine ip frontend)" application
 
 # No need to start more Consul instances on the nodes as overlay networking abstracts the nodes away
-
-# Start registrator
-#docker-1.9 run -d -v /var/run/docker.sock:/tmp/docker.sock -h registrator --name registrator gliderlabs/registrator -internal consul://`docker-machine ip infra`:8500
 
 unset VIRTUALBOX_BOOT2DOCKER_URL
--- experimental-1.9/commands-for-swarm-overlay.sh
+++ experimental-1.9/commands-for-swarm-overlay.sh
@@ -24,26 +24,30 @@ docker $(docker-machine config swarm-0) run -d --restart="always" --net="bridge"
 # NB! This is different from the example as the certificates are passed directly from the host
 
 # Start Consul inside the Swarm with the swarm-master in server mode
-docker-1.9 $(docker-machine config swarm-0) run -d --name consul -p 8500:8500 progrium/consul -server -bootstrap-expect 1
+docker $(docker-machine config swarm-0) run -d --name consul -p 8500:8500 progrium/consul -server -bootstrap-expect 1
 
-# Start swarm node
+# Start registrator on the first node, point it to the just created Consul instance
+docker $(docker-machine config swarm-0) run -d -v /var/run/docker.sock:/tmp/docker.sock -h registrator --name registrator-swarm-0 gliderlabs/registrator -internal consul://consul:8500
+
+# Start second swarm node
 docker-machine create -d virtualbox --engine-opt="default-network=overlay:multihost" --engine-opt="kv-store=consul:$(docker-machine ip infra):8500" --engine-label="com.docker.network.driver.overlay.bind_interface=eth1" --engine-label="com.docker.network.driver.overlay.neighbor_ip=$(docker-machine ip swarm-0)" --engine-insecure-registry $(docker-machine ip infra):5000 swarm-1
 
 docker $(docker-machine config swarm-1) run -d --restart="always" --net="bridge" swarm:latest join --addr "$(docker-machine ip swarm-1):2376" "token://$SWARM_TOKEN"
 
-# TODO: Start Consul agent on the node, not really needed any more as overlay networking abstracts this away
+# Start registrator on the second node, point to the same Consul
+docker $(docker-machine config swarm-1) run -d -v /var/run/docker.sock:/tmp/docker.sock -h registrator --name registrator-swarm-1 gliderlabs/registrator -internal consul://consul:8500
+
+# TODO: Starting Consul agent on the node is not really needed any more as overlay networking abstracts this away
 # If registrator is suitable, then more Consuls might make sense named the same as the swarm machine they're on
 # The registrator on the node would point to the local Consul instance
 
 # Point Docker at swarm
-export DOCKER_HOST=tcp://"$(docker-machine ip swarm-0):3376"
+export DOCKER_HOST=tcp://$(docker-machine ip swarm-0):3376
 export DOCKER_TLS_VERIFY=1
 export DOCKER_CERT_PATH="$HOME/.docker/machine/machines/swarm-0"
 
 # Start haproxy
-docker-1.9 run -d --name=rest -p 80:80 -p 1936:1936 sirile/haproxy -consul=consul:8500
+docker run -d --name=rest -p 80:80 -p 1936:1936 sirile/haproxy -consul=consul:8500
 
-# Start example services, repeat these a few times
-CONTAINER=$(docker run -d --dns 172.17.42.1 sirile/node-image-test)
-# Register service to Consul, registrator in not (yet) suitable for this
-curl -X post -d '{"Name":"test","ID":"'$CONTAINER'","Tags":["rest"],"Address":"'$(docker inspect --format="{{.NetworkSettings.IPAddress}}" $CONTAINER)'", "port":80}' http://$(docker inspect --format='{{.Node.IP}}' consul):8500/v1/agent/service/register
+# Start example services, repeat this a few times
+docker run -d -e SERVICE_NAME=test -e SERVICE_TAGS=rest --dns 172.17.42.1 -p :80 sirile/node-image-test
